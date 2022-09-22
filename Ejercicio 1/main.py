98cd2818d26499a27002c4be66af47403787bf80
Alexander Nuñez <82907784+Alerxp@users.noreply.github.com>
2022-09-22 16:17:12 -0300

    Add files via upload

---
+++ Ejercicio 1/main.py
@@ -0,0 +1,13 @@
+from ABB import ABB
+
+if __name__ == '__main__':
+    arbol = ABB()
+    arbol.insertar(50)
+    arbol.insertar(80)
+    arbol.insertar(30)
+    arbol.insertar(10)
+    arbol.insertar(35)
+    arbol.insertar(40)
+    arbol.insertar(60)
+    arbol.insertar(95)
+    arbol.insertar(71)
